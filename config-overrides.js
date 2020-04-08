b94c6569786fa397c96c594284a31b8df9531ff1
winsonfeng <807965894@qq.com>
2020-04-08 12:42:38 +0800

    'fisrt'

---
+++ config-overrides.js
@@ -0,0 +1,26 @@
+/*
+* 基于customize 和 react-app-rewired的定制化配置文件
+* */
+
+// 引入相关的方法
+const {
+    override,
+    addLessLoader,/*引入less方法*/
+    fixBabelImports/*按需加载antd*/
+} = require('customize-cra')
+
+const modifyVars = require('./modifyVars')
+
+module.exports = override(
+
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }),
+    //引入less
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars
+    })
+)
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Button} from 'antd'
+import './css/index.less'
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Button type="primary">666</Button>
+            </div>
+        );
+    }
+}
+
+export default App;
